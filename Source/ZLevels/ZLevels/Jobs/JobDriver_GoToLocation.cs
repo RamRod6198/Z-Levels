2033e1585b9f34a14f6d0d303fa77a5655b7c0c3
IsaacSherman <sherman1337@gmail.com>
2020-09-05 19:03:40 -0400

    Fixed a bug and introduced a nicer, smaller bug in its place.  This one makes you walk to the nearest stairs before you go to your destination.  Should be a relatively easy fix, but I want to get merged and then I'll finish up.

--- Source/ZLevels/ZLevels/Jobs/JobDriver_GoToLocation.cs
+++ Source/ZLevels/ZLevels/Jobs/JobDriver_GoToLocation.cs
@@ -13,8 +13,7 @@ namespace ZLevels
     public class JobDriver_GoToLocation : JobDriver
     {
         //Target A = destination
-        //Target B = sink stairs (closest to destination
-        //Target C = source stairs (closest to pawn)  
+        //Since this handles clicks, this is only built to handle intra-map paths.
         public override bool TryMakePreToilReservations(bool errorOnFailed)
         {
             return true;
@@ -27,44 +26,11 @@ namespace ZLevels
             //yield break if not.
             //Otherwise, calculate toils to go, then toils to destination and return them one at a time.
 
-            List<ZPathfinder.DijkstraGraph.Node> stairList =
-                ZPathfinder.Instance.FindRoute(pawn.Position, TargetLocA, pawn.Map, TargetB.Thing.Map, out float routeCost);
-            yield return new Toil { initAction = delegate
+
+            foreach (var v in Toils_ZLevels.GoToDestinationUsingStairs(pawn, TargetA.Cell, pawn.Map, this))
             {
-                ZLogger.Message(
-                    $"Target  a = {TargetA.Thing}, target B = {TargetB.Thing}, Target c = {TargetC.Thing}"); } };
-            yield return new Toil
-            { initAction = delegate () {
-                for (int j = 0; j < stairList.Count; ++j)
-                {
-                    ZLogger.Message($"Stairs {j} = {stairList[j]}");
-                } } };            
-
-            for (int i = 0; i < stairList.Count - 1; ++i)
-            {
-                int i1 = i;
-                yield return new Toil
-                { initAction = delegate () { ZLogger.Message($"Going to target {i1} boss"); } };
-            
-                Toil setStairs = Toils_ZLevels.GetSetStairs(pawn, stairList[i+1].Map, this);
-                Toil useStairs = Toils_General.Wait(60, 0);
-                useStairs.WithProgressBarToilDelay(TargetIndex.A);
-
-                yield return setStairs;
-                yield return Toils_Goto.GotoCell(stairList[i].Location, PathEndMode.OnCell);
-                yield return useStairs;
-
-
-                yield return Toils_ZLevels.GetTeleport(pawn, stairList[i1+1].Map, this, setStairs);
-
-
-                //foreach (Toil t in Toils_ZLevels.GoToMap(pawn, stairList[i1 + 1].Map, this))
-                //{
-                //    yield return t;
-                //}
-
+                yield return v;
             }
-            yield return Toils_Goto.GotoThing(TargetIndex.A, PathEndMode.OnCell);
         }
 
 
